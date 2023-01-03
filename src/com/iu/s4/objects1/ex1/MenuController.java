506aeefe965c5b99e43160e26857c40424c9d005
Daeki <kimdaeki@gmail.com>
2023-01-03 10:25:07 +0900

    1

--- src/com/iu/s4/objects1/ex1/MenuController.java
+++ src/com/iu/s4/objects1/ex1/MenuController.java
@@ -14,23 +14,31 @@ public class MenuController {
 	public void start() {
 		Scanner sc = new Scanner(System.in);
 		boolean check=true;
+		StudentInput si = new StudentInput();
+		StudentView sv = new StudentView();
+		
+		System.out.println("학생수를 입력하세요");
+		int count = sc.nextInt();
+		Student [] students = new Student[count];
+		
 		
 		while(check) {
-			System.out.println("1.등록");
+			System.out.println("1.학생 정보 입력");
 			System.out.println("2.수정");
-			System.out.println("3.삭제");
+			System.out.println("3.학생 전체 정보 출력");
 			System.out.println("4.종료");
 			int select=sc.nextInt();
 			
 			switch(select) {
 			case 1:
 				System.out.println("등록입니다");
+				si.setAllStudents(students);
 				break;
 			case 2:
 				System.out.println("수정입니다");
 				break;
 			case 3:
-				System.out.println("삭제입니다");
+				sv.viewList(students);
 				break;
 			default:
 				System.out.println("종료합니다");
--- src/com/iu/s4/objects1/ex1/StudentInput.java
+++ src/com/iu/s4/objects1/ex1/StudentInput.java
@@ -1,15 +1,60 @@
 package com.iu.s4.objects1.ex1;
 
+import java.util.Scanner;
+
 public class StudentInput {
 	
 	//setStudent
 	//학생한명 객체를 생성해서, 이름, 번호, 국어, 영어, 수학 입력
 	//총점 평균
+	public void setStudent() {
+		Scanner sc = new Scanner(System.in);
+		Student student = new Student();
+		System.out.println("이름 입력");
+		student.name=sc.next();
+		System.out.println("번호 입력");
+		student.num = sc.nextInt();
+		System.out.println("국어 입력");
+		student.kor = sc.nextInt();
+		System.out.println("영어 입력");
+		student.eng = sc.nextInt();
+		System.out.println("수학 입력");
+		student.math = sc.nextInt();
+		
+		student.total=student.kor+student.eng+student.math;
+		student.avg = student.total/3.0;
+		
+		
+	}
+	
 	
 	//setAllStudents
 	//매개변수로 학생배열을 받아서
 	//배열의 수만큼 학생 객체를 생성해서 이름, 번호, 국어, 영어, 수학 입력
 	//총점 평균
 	//학생 객체를 배열에 대입
+	public void setAllStudents(Student [] students) {
+		Scanner sc = new Scanner(System.in);
+		for(int i=0;i<students.length;i++) {
+			Student student = new Student();
+			System.out.println("이름 입력");
+			student.name=sc.next();
+			System.out.println("번호 입력");
+			student.num = sc.nextInt();
+			System.out.println("국어 입력");
+			student.kor = sc.nextInt();
+			System.out.println("영어 입력");
+			student.eng = sc.nextInt();
+			System.out.println("수학 입력");
+			student.math = sc.nextInt();
+			
+			student.total=student.kor+student.eng+student.math;
+			student.avg = student.total/3.0;
+			
+			students[i]=student;
+			
+		}
+		
+	}
 
 }
